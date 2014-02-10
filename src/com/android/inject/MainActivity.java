f10515fb90d275443b43ced70dec036f032b9d20
Wenzhao <wzhfang@marvell.com>
2014-02-10 19:49:18 +0800

    reduce version to match v2.3
    Signed-off-by: Wenzhao <wzhfang@marvell.com>

--- src/com/android/inject/MainActivity.java
+++ src/com/android/inject/MainActivity.java
@@ -2,7 +2,6 @@ package com.android.inject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -11,13 +10,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RootImpl.startInject(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
 }
